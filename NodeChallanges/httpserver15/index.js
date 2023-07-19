8d119d20af610d9620cfe8c984c18cb3cd9ddf56
kamranlatifch <kamranlatif98@gmail.com>
2023-07-19 17:16:39 +0500

    httpsServer API Data display

--- NodeChallanges/httpserver15/index.js
+++ NodeChallanges/httpserver15/index.js
@@ -1,7 +1,33 @@
-//Use of http module
+//Use of http module lec#16
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 const server = http.createServer((req, res) => {
-  res.end("Hello From Kamran's Server");
+  const data = fs.readFileSync("../UserAPI/userapi.json", "utf-8");
+  const objData = JSON.parse(data);
+
+  if (req.url === "/") {
+    res.end("Hello From Kamran's Server");
+  } else if (req.url === "/about") {
+    res.end("Hello From About Us");
+  } else if (req.url === "/contact") {
+    res.write("Hello From Contact Us");
+    res.end();
+    //200
+  } else if (req.url === "/userapi") {
+    // const apiPath = path.join(__dirname, "../UserAPI/userapi.json");
+
+    // fs.readFile("../UserAPI/userapi.json", "utf-8", (err, data) => {
+    //   console.log(data);
+
+    //   res.end(data);
+    // });
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404);
+    res.end("Error:404, Page Not Found");
+  }
 });
 
 server.listen(3000, "127.0.0.1", () => {
